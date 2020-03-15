30f2d77cf775dbdd9158463fe50d705b30355b8f
Nezar <hamideh@yandex.ru>
2020-03-15 19:57:22 +0300

    annotations_update1
    изменен css аннотаций, js

--- public/js/markup3dExtension.js
+++ public/js/markup3dExtension.js
@@ -29,10 +29,10 @@ class Markup3dExtension extends Autodesk.Viewing.Extension {
                 this._group = new Autodesk.Viewing.UI.ControlGroup('allMyAwesomeExtensionsToolbar');
                 this.viewer.toolbar.addControl(this._group);
             }
-            var panel = this.panel;
-            var index = 0;
-            var html_str;
-            var renderer = viewer.impl.renderer();
+            let panel = this.panel;
+            let index = 1;
+            let html_str;
+            let renderer = viewer.impl.renderer();
             // Add a new button to the toolbar group
             this._button = new Autodesk.Viewing.UI.Button('Markup3dExtensionButton');
             this._button.onClick = () => {
@@ -43,11 +43,9 @@ class Markup3dExtension extends Autodesk.Viewing.Extension {
                         'Markup3dExtensinPanel', 'Пометка');
                 }
                 // show/hide docking panel
-                // var object = [];
-                var particle = [];
-                var screenpoint;
+                let particle = [];
+                let screenpoint;
                 panel.setVisible(!panel.isVisible());
-
                 document.querySelector("#forgeViewer").addEventListener('click', e => {
                     console.log(e.clientX - $("#left-col").outerWidth(), e.clientY);
                     particle[index] = viewer.clientToWorld(e.clientX - $("#left-col").outerWidth(), e.clientY);
@@ -58,29 +56,18 @@ class Markup3dExtension extends Autodesk.Viewing.Extension {
                         if (text == 'undefined')
                             text = "";
                         html_str += '<div class="annotation" id="annotation_' + index + '"' + 'style = "top: ' + screenpoint.y + 'px; left: ' + screenpoint.x + 'px;"' +
-                            '>' + '<span class="annotationIndex">' + index + '</span>' + '<p>' + text + '</p></div>';
+                            '>' + '<span class="annotationIndex">' + index + '</span>' + '<span>' + text + '</span></div>';
                         $("#annotations").html(html_str);
                         index++;
                         viewer.clearSelection();
                         document.getElementById("markuptext").value = "";
                         updateScreenPosition();
-
                     }
-
                 })
 
                 function updateScreenPosition() {
-                    for (let i = 0; i < index; i++) {
-                        //var vector = new THREE.Vector3(object[index]);
-                        //canvas = renderer.domElement;
-
-                        //vector.project(viewer.impl.camera);
-                        console.log(i);
-                        console.log(particle[i].point.x, particle[i].point.y, particle[i].point.z);
+                    for (let i = 1; i < index; i++) {
                         screenpoint = viewer.impl.worldToClient(new THREE.Vector3(particle[i].point.x, particle[i].point.y, particle[i].point.z), viewer.impl.camera);
-
-                        //screenpoint.x = Math.round((0.5 + screenpoint.x / 2) * (window.innerWidth / window.devicePixelRatio));
-                        //screenpoint.y = Math.round((0.5 - screenpoint.y / 2) * (window.innerHeight / window.devicePixelRatio));
                         let anid = '#annotation_' + i;
                         $(anid).css('top', screenpoint.y);
                         $(anid).css('left', screenpoint.x);
@@ -110,31 +97,20 @@ function TextPanel(viewer, container, id, title, options) {
     // the style of the docking panel
     // use this built-in style to support Themes on Viewer 4+
     this.container.classList.add('docking-panel-container-solid-color-a');
+    this.container.classList.add('annotation-panel');
     this.container.style.top = "180px";
     this.container.style.right = "15px";
-    this.container.style.width = "250px";
-    this.container.style.height = "350px";
     this.container.style.resize = "none";
+    this.container.style.minWidth = "250px";
+    this.container.style.minHeight = "330px";
 
     // this is where we should place the content of our panel
     var textarea = document.createElement('textarea');
+    textarea.classList.add('text-annotation');
     textarea.placeholder = 'Введите текст пометки';
-    textarea.style.borderRadius = '3px';
-    textarea.style.width = '240px';
-    textarea.style.height = '265px';
-    textarea.style.marginTop = '10px';
-    textarea.style.marginLeft = '3px';
-    textarea.style.marginRight = '3px';
-    textarea.style.color = 'black';
-    textarea.style.resize = 'none';
-    textarea.style.fontSize = '18px';
-    textarea.style.fontFamily = 'TimesNewRoman';
-    textarea.style.padding = '2px';
-    textarea.style.border = 'none';
     textarea.id = 'markuptext';
     this.container.appendChild(textarea);
 }
 TextPanel.prototype = Object.create(Autodesk.Viewing.UI.DockingPanel.prototype);
 TextPanel.prototype.constructor = TextPanel;
-
 Autodesk.Viewing.theExtensionManager.registerExtension('Markup3dExtension', Markup3dExtension);
