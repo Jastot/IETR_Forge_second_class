3fcdd73d3a76f80f85243677ae54b267f047fd94
Nezar <hamideh@yandex.ru>
2020-04-02 16:30:38 +0300

    componentsV2.1

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -1,19 +1,5 @@
 var viewer;
 
-$(document).ready(function() {
-    var urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZS5zdHA=';
-    getForgeToken(function(access_token) {
-        jQuery.ajax({
-            url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn + '/manifest',
-            headers: { 'Authorization': 'Bearer ' + access_token },
-            success: function(res) {
-                if (res.status === 'success') launchViewer(urn);
-                else $("#forgeViewer").html('Преобразование всё ещё выполняется').css('color', 'white');
-            }
-        });
-    });
-});
-
 function launchViewer(urn) {
     var options = {
         env: 'AutodeskProduction',
@@ -27,6 +13,9 @@ function launchViewer(urn) {
         Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
     });
 }
+let check = false;
+var components;
+var comp_data;
 
 function onDocumentLoadSuccess(doc) {
     var viewables = doc.getRoot().getDefaultGeometry();
@@ -38,71 +27,19 @@ function onDocumentLoadSuccess(doc) {
         // console.log(ext);
         // console.log(ext.isActive());
         viewer.addEventListener(Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT, function() {
-<<<<<<< Updated upstream
-            var components = buildModelTree(viewer.model);
-            console.log(components);
-            prepareTree(components);
-            $('#testtree2').jstree(true).refresh();
-=======
             if (check == false) {
                 components = buildModelTree(viewer.model);
+                console.log(components);
                 comp_data = components;
+                console.log(comp_data.children);
                 let chi = get_children(comp_data.children);
                 $('#testtree').jstree(true).settings.core.data = get_new_data(chi);
+                console.log(chi);
                 $('#testtree').jstree("refresh");
             }
             check = true;
->>>>>>> Stashed changes
         })
     });
-}
-
-function get_new_data(child_data) {
-    return [{
-            text: 'Общие сведения',
-            type: 'info',
-            state: {},
-            id: 'info'
-        }, {
-            text: 'Компоненты',
-            type: 'components',
-            children: child_data
-
-        },
-        {
-            text: 'Принцип работы',
-            type: 'work',
-            id: 'work'
-        },
-        {
-            text: 'Обслуживание',
-            type: 'service',
-            children: [{
-                text: 'service1',
-                type: 'child'
-            }, {
-                text: 'service2',
-                type: 'child'
-            }],
-            id: 'service'
-        }
-    ];
-}
-
-function get_children(arr) {
-    let clone = [];
-    for (const i in arr) {
-        if (arr[i].children instanceof Array && arr[i].children.length > 1) {
-            clone[i] = {
-                text: `${arr[i].name}`,
-                children: get_children(arr[i].children)
-            };
-            continue;
-        }
-        if (arr[i].name != "Solid1")
-            clone[i] = { text: `${arr[i].name}` };
-    }
-    return clone;
 }
 
 function getAlldbIds(rootId) {
@@ -136,6 +73,7 @@ function getForgeToken(callback) {
 
 function getActiveConfigurationProperties(viewer) {
     var dbIds = viewer.getSelection();
+
     if (dbIds.length !== 1) {
         alert("Сначала выберите один элемент!");
         return;
@@ -147,8 +85,63 @@ function getActiveConfigurationProperties(viewer) {
                 viewer.getProperties(prop.displayValue, confProps => {
                     console.log(confProps);
                 });
+
                 return;
             }
         })
     })
+}
+
+
+function get_new_data(child_data) {
+    return [{
+            text: 'Общие сведения',
+            type: 'info',
+            state: {},
+            id: 'info'
+        }, {
+            text: 'Компоненты',
+            type: 'components',
+            children: child_data
+
+        },
+        {
+            text: 'Дурка',
+            type: 'components',
+            id: 'durka'
+        },
+        {
+            text: 'Принцип работы',
+            type: 'work',
+            id: 'work'
+        },
+        {
+            text: 'Обслуживание',
+            type: 'service',
+            children: [{
+                text: 123,
+                type: 'child'
+            }, {
+                text: 321,
+                type: 'child'
+            }],
+            id: 'service'
+        }
+    ];
+}
+
+function get_children(arr) {
+    let clone = [];
+    for (const i in arr) {
+        if (arr[i].children instanceof Array && arr[i].children.length > 1) {
+            clone[i] = {
+                text: `${arr[i].name}`,
+                children: get_children(arr[i].children)
+            };
+            continue;
+        }
+        if (arr[i].name != "Solid1")
+            clone[i] = { text: `${arr[i].name}` };
+    }
+    return clone;
 }
--- public/js/testtree.js
+++ public/js/testtree.js
@@ -1,44 +1,7 @@
 $(document).ready(function() {
-    prepareTree();
+    prepareTree(null);
 });
 
-<<<<<<< Updated upstream
-function prepareTree(components) {
-    if (components == null) {
-        $('#testtree').jstree({
-            'core': {
-                'multiple': false,
-                'check_callback': true,
-                'themes': { "icons": true },
-                'data': [{
-                        text: 'Общие сведения',
-                        type: 'info',
-                        state: {},
-                        id: 'info'
-                    }, {
-                        text: 'Компоненты',
-                        type: 'components',
-                        children: {},
-                        id: 'components'
-                    },
-                    {
-                        text: 'Принцип работы',
-                        type: 'work',
-                        id: 'work'
-                    }, {
-                        text: 'Обслуживание',
-                        type: 'service',
-                        children: [{
-                            text: 123,
-                            type: 'child'
-                        }, {
-                            text: 321,
-                            type: 'child'
-                        }],
-                        id: 'service'
-                    }
-                ]
-=======
 function prepareTree() {
 
     $('#testtree').jstree({
@@ -49,13 +12,12 @@ function prepareTree() {
             'data': [{
                     text: 'Общие сведения',
                     type: 'info',
-                    state: {
-                        selected: true
-                    },
+                    state: {},
                     id: 'info'
                 }, {
                     text: 'Компоненты',
                     type: 'components',
+                    children: {},
                     id: 'components'
                 },
                 {
@@ -78,127 +40,41 @@ function prepareTree() {
         },
         "types": {
             "default": {
-                "icon": false
+                "icon": "glyphicon glyphicon-flash"
             },
             "info": {
                 "icon": "glyphicon glyphicon-book"
->>>>>>> Stashed changes
             },
-            "types": {
-                "default": {
-                    "icon": "glyphicon glyphicon-flash"
-                },
-                "info": {
-                    "icon": "glyphicon glyphicon-book"
-                },
-                "work": {
-                    "icon": "glyphicon glyphicon-cog"
-                },
-                "components": {
-                    "icon": "glyphicon glyphicon-list-alt"
-                },
-                "service": {
-                    "icon": "glyphicon glyphicon-wrench"
-                },
-                "child": {
-                    "icon": "glyphicon glyphicon-menu-right"
-                }
+            "work": {
+                "icon": "glyphicon glyphicon-cog"
             },
-            "plugins": ["types"]
-        }).bind("activate_node.jstree", function(evt, data) {
-            if (data != null && data.node != null) {
-                $("#forgeViewer").empty();
-                var urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZS5zdHA=';
-                getForgeToken(function(access_token) {
-                    jQuery.ajax({
-                        url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn + '/manifest',
-                        headers: { 'Authorization': 'Bearer ' + access_token },
-                        success: function(res) {
-                            if (res.status === 'success') launchViewer(urn);
-                            else $("#forgeViewer").html('Преобразование всё ещё выполняется').css('color', 'white');
-                        }
-                    });
-                })
+            "components": {
+                "icon": "glyphicon glyphicon-list-alt"
+            },
+            "service": {
+                "icon": "glyphicon glyphicon-wrench"
+            },
+            "child": {
+                "icon": "glyphicon glyphicon-menu-right"
             }
-        });
-    } else {
-        $('#testtree2').jstree({
-            'core': {
-                'multiple': false,
-                'check_callback': true,
-                'themes': { "icons": true },
-                'data': [{
-                        text: 'Общие сведения',
-                        type: 'info',
-                        state: {},
-                        id: 'info'
-                    }, {
-                        text: 'Компоненты',
-                        type: 'components',
-                        children: function() {
-                            components.forEach((item) => {
-                                console.log(5);
-                                return { 'text': item.name };
-                            });
-                        },
-                        id: 'components'
-                    },
-                    {
-                        text: 'Принцип работы',
-                        type: 'work',
-                        id: 'work'
-                    }, {
-                        text: 'Обслуживание',
-                        type: 'service',
-                        children: [{
-                            text: 123,
-                            type: 'child'
-                        }, {
-                            text: 321,
-                            type: 'child'
-                        }],
-                        id: 'service'
+        },
+        "plugins": ["types"]
+    }).bind("activate_node.jstree", function(evt, data) {
+        if (data != null && data.node != null) {
+            $("#forgeViewer").empty();
+            var urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZS5zdHA=';
+            getForgeToken(function(access_token) {
+                jQuery.ajax({
+                    url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn + '/manifest',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    success: function(res) {
+                        if (res.status === 'success') launchViewer(urn);
+                        else $("#forgeViewer").html('Преобразование всё ещё выполняется').css('color', 'white');
                     }
-                ]
-            },
-            "types": {
-                "default": {
-                    "icon": "glyphicon glyphicon-flash"
-                },
-                "info": {
-                    "icon": "glyphicon glyphicon-book"
-                },
-                "work": {
-                    "icon": "glyphicon glyphicon-cog"
-                },
-                "components": {
-                    "icon": "glyphicon glyphicon-list-alt"
-                },
-                "service": {
-                    "icon": "glyphicon glyphicon-wrench"
-                },
-                "child": {
-                    "icon": "glyphicon glyphicon-menu-right"
-                }
-            },
-            "plugins": ["types"]
-        }).bind("activate_node.jstree", function(evt, data) {
-            if (data != null && data.node != null) {
-                $("#forgeViewer").empty();
-                var urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZS5zdHA=';
-                getForgeToken(function(access_token) {
-                    jQuery.ajax({
-                        url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn + '/manifest',
-                        headers: { 'Authorization': 'Bearer ' + access_token },
-                        success: function(res) {
-                            if (res.status === 'success') launchViewer(urn);
-                            else $("#forgeViewer").html('Преобразование всё ещё выполняется').css('color', 'white');
-                        }
-                    });
-                })
-            }
-        });
-    }
+                });
+            })
+        }
+    });
 
 }
 
@@ -209,18 +85,25 @@ function buildModelTree(model, createNodeFunc = null) {
 
         instanceTree.enumNodeChildren(node.dbId,
             function(childId) {
+
                 var childNode = null;
+
                 if (createNodeFunc) {
+
                     childNode = createNodeFunc(childId);
+
                 } else {
+
                     node.children = node.children || [];
 
                     childNode = {
                         dbId: childId,
                         name: instanceTree.getNodeName(childId)
                     }
+
                     node.children.push(childNode);
                 }
+
                 _buildModelTreeRec(childNode);
             });
     }
