61ca4cbcf23e10b7ae2b13aeb1831c36863c9a0e
Nezar <hamideh@yandex.ru>
2020-04-27 14:20:42 +0300

    fusion_animationsV1

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -1,13 +1,13 @@
 var viewer;
-$(document).ready(function() {
+$(document).ready(function () {
     $("#forgeViewer").empty();
     var urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZS5zdHA=';
     var urn2 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL1Rlc3QuZjNk';
-    getForgeToken(function(access_token) {
+    getForgeToken(function (access_token) {
         jQuery.ajax({
             url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn2 + '/manifest',
             headers: { 'Authorization': 'Bearer ' + access_token },
-            success: function(res) {
+            success: function (res) {
                 if (res.status === 'success') launchViewer(urn2);
                 else $("#forgeViewer").html('Преобразование всё ещё выполняется').css('color', 'lightblue');
             }
@@ -24,13 +24,12 @@ function launchViewer(urn) {
     Autodesk.Viewing.Initializer(options, () => {
         viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), {
             extensions: ['HandleSelectionExtension', 'Markup3dExtension']
-                //disabledExtensions: { explode: true, bimwalk: true, settings: true, propertiesmanager: true, modelstructure: true }
+            //disabledExtensions: { explode: true, bimwalk: true, settings: true, propertiesmanager: true, modelstructure: true }
         });
         viewer.start();
         viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
         var documentId = 'urn:' + urn;
         Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
-        console.log(viewer);
 
 
 
@@ -102,21 +101,17 @@ var comp_data;
 //         Autodesk.Viewing.TOOLBAR_CREATED_EVENT,
 //         onToolbarCreated)
 // }
-
 function onDocumentLoadSuccess(doc) {
-    console.log(doc);
     var animationItems = [];
     if (animationItems.length == 0) {
-        animationItems = Autodesk.Viewing.Document.getSubItemsWithProperties(doc.getRoot(), {
+        animationItems = doc.getRoot().search({
             'type': 'folder',
             'role': 'animation'
         }, true);
     }
     if (animationItems.length > 0) {
-        viewer.load(doc.getViewablePath(animationItems[0].children[0]));
+        viewer.loadModel(doc.getViewablePath(animationItems[0].children[0]));
     }
-
-    console.log(animationItems);
 
 
 
@@ -126,9 +121,9 @@ function onDocumentLoadSuccess(doc) {
         // viewer.loadExtension('Autodesk.ModelStructure');
         // let ext = viewer.getExtension('Autodesk.ModelStructure');
         // ext.activate();
-        viewer.addEventListener(Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT, function() {
+        viewer.addEventListener(Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT, function () {
             var isolated;
-            $("#compTree").on("open_node.jstree", function(e, data) {
+            $("#compTree").on("open_node.jstree", function (e, data) {
                 if (data.node.id === 'components') {
                     var row = $(".row").children();
                     $(row[0]).removeClass('col-sm-2 col-md-2').addClass('col-sm-3 col-md-3');
@@ -136,7 +131,7 @@ function onDocumentLoadSuccess(doc) {
                     viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
                 }
             });
-            $("#compTree").on("close_node.jstree", function(e, data) {
+            $("#compTree").on("close_node.jstree", function (e, data) {
                 if (data.node.id === 'components') {
                     var row = $(".row").children();
                     $(row[0]).removeClass('col-sm-3 col-md-3').addClass('col-sm-2 col-md-2');
@@ -144,21 +139,21 @@ function onDocumentLoadSuccess(doc) {
                     viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
                 }
             });
-            $('#compTree').on("activate_node.jstree", function(evt, data) {
+            $('#compTree').on("activate_node.jstree", function (evt, data) {
                 if (data != null && data.node != null && data.node.type == 'object') {
                     let dbid = data.node.id.substring(data.node.id.lastIndexOf('_') + 1);
                     if (isolated != dbid) {
                         $.ajax({
                             url: '/texts/' + dbid,
                             type: 'GET',
-                            success: function(res) {
+                            success: function (res) {
                                 let name = res.name;
                                 let text = res.text;
                                 console.log(name);
                                 console.log(text);
                                 adjustLayout(name, text);
                             },
-                            error: function(err) {
+                            error: function (err) {
                                 console.log(err);
                             }
                         });
@@ -170,11 +165,22 @@ function onDocumentLoadSuccess(doc) {
             });
         });
 
-        viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, function() {
+        viewer.addEventListener(Autodesk.Viewing.GEOMETRY_LOADED_EVENT, function () {
+            var scene = viewer.impl.modelQueue();
+            var model = scene.findModel(1);
+            console.log(model);
+            // remove model from viewer - but without discarding materials
+            viewer.impl.removeModel(model);
 
+            // make this model available for later showModel() calls
+            scene.addHiddenModel(model);
+            console.log(viewer);
             viewer.loadExtension('Autodesk.Fusion360.Animation');
-            var aExt = viewer.getExtension('Autodesk.Fusion360.Animation');
-            console.log(aExt)
+            setTimeout(() => {
+                var aExt = viewer.getExtension('Autodesk.Fusion360.Animation');
+                console.log(aExt);
+                aExt.load();
+            }, 3000);
 
             $("#cube-loader").addClass("loaded_hiding");
             setTimeout(() => {
@@ -194,7 +200,7 @@ function getAlldbIds(rootId) {
     while (queue.length > 0) {
         var node = queue.shift();
         alldbId.push(node);
-        instanceTree.enumNodeChildren(node, function(childrenIds) {
+        instanceTree.enumNodeChildren(node, function (childrenIds) {
             queue.push(childrenIds);
         });
     }
