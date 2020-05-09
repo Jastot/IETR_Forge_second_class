7e69e9e919c688ff0ab921af58b73162da06ba44
Nezar <hamideh@yandex.ru>
2020-05-09 20:40:17 +0300

    model_change

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -1,14 +1,15 @@
 var viewer;
-$(document).ready(function() {
+$(document).ready(function () {
     $("#forgeViewer").empty();
     var urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZS5zdHA=';
     var urn2 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL1Rlc3QuZjNk';
-    getForgeToken(function(access_token) {
+    var urn3 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZTIyMi5mM2Q=';
+    getForgeToken(function (access_token) {
         jQuery.ajax({
-            url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn2 + '/manifest',
+            url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn3 + '/manifest',
             headers: { 'Authorization': 'Bearer ' + access_token },
-            success: function(res) {
-                if (res.status === 'success') launchViewer(urn2);
+            success: function (res) {
+                if (res.status === 'success') launchViewer(urn3);
                 else $("#forgeViewer").html('Преобразование всё ещё выполняется').css('color', 'lightblue');
             }
         });
@@ -24,7 +25,7 @@ function launchViewer(urn) {
     Autodesk.Viewing.Initializer(options, () => {
         viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), {
             extensions: ['HandleSelectionExtension', 'Markup3dExtension', 'Autodesk.Fusion360.Animation']
-                //disabledExtensions: { explode: true, bimwalk: true, settings: true, propertiesmanager: true, modelstructure: true }
+            //disabledExtensions: { explode: true, bimwalk: true, settings: true, propertiesmanager: true, modelstructure: true }
         });
         viewer.start();
         viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
@@ -41,11 +42,13 @@ function onDocumentLoadSuccess(doc) {
 
         var animationItems = [];
         if (animationItems.length == 0) {
+            console.log(doc.getRoot());
             animationItems = doc.getRoot().search({
                 'type': 'folder',
                 'role': 'animation'
             }, true);
         }
+        console.log(animationItems);
         if (animationItems.length > 0) {
             viewer.loadModel(doc.getViewablePath(animationItems[0].children[0]), () => {
                 viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
@@ -74,7 +77,7 @@ function treeEvents() {
     var isolated;
     var lastNode;
 
-    $("#compTree").on("open_node.jstree", function(e, data) {
+    $("#compTree").on("open_node.jstree", function (e, data) {
         if (data.node.id === 'components') {
             var row = $(".row").children();
             $(row[0]).removeClass('col-sm-2 col-md-2').addClass('col-sm-3 col-md-3');
@@ -82,7 +85,7 @@ function treeEvents() {
         }
     });
 
-    $("#compTree").on("close_node.jstree", function(e, data) {
+    $("#compTree").on("close_node.jstree", function (e, data) {
         if (data.node.id === 'components') {
             var row = $(".row").children();
             $(row[0]).removeClass('col-sm-3 col-md-3').addClass('col-sm-2 col-md-2');
@@ -90,16 +93,16 @@ function treeEvents() {
         }
     });
 
-    $('#compTree').on("activate_node.jstree", function(evt, data) {
+    $('#compTree').on("activate_node.jstree", function (evt, data) {
         if (data != null && data.node != null) {
             $.ajax({
                 url: '/model_id',
                 type: 'GET',
                 data: { 'type': data.node.type },
-                success: function(res) {
+                success: function (res) {
                     getModel(Number(res));
                 },
-                error: function(err) {
+                error: function (err) {
                     console.log(err);
                 }
             });
@@ -110,12 +113,12 @@ function treeEvents() {
                     $.ajax({
                         url: '/texts/' + dbid,
                         type: 'GET',
-                        success: function(res) {
+                        success: function (res) {
                             let name = res.name;
                             let text = res.text;
                             adjustLayout(name, text);
                         },
-                        error: function(err) {
+                        error: function (err) {
                             console.log(err);
                         }
                     });
@@ -132,12 +135,12 @@ function treeEvents() {
                     url: '/tree/texts',
                     type: 'GET',
                     data: { 'type': data.node.type },
-                    success: function(res) {
+                    success: function (res) {
                         let name = res.name;
                         let text = res.text;
                         adjustLayout(name, text);
                     },
-                    error: function(err) {
+                    error: function (err) {
                         console.log(err);
                     }
                 });
@@ -182,7 +185,7 @@ function getAlldbIds(rootId) {
     while (queue.length > 0) {
         var node = queue.shift();
         alldbId.push(node);
-        instanceTree.enumNodeChildren(node, function(childrenIds) {
+        instanceTree.enumNodeChildren(node, function (childrenIds) {
             queue.push(childrenIds);
         });
     }
