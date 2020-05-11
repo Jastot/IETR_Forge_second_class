7d0eceb62e218103085282ad938c19ed5bb0b3f1
Nezar <hamideh@yandex.ru>
2020-05-12 02:51:42 +0300

    animtext_changes

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -1,15 +1,15 @@
 var viewer;
-$(document).ready(function() {
+$(document).ready(function () {
     $("#forgeViewer").empty();
     var urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZS5zdHA=';
     var urn2 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL1Rlc3QuZjNk';
     var urn3 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZTIyMi5mM2Q=';
     var urn4 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZUxhc3QuZjNk';
-    getForgeToken(function(access_token) {
+    getForgeToken(function (access_token) {
         jQuery.ajax({
             url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn4 + '/manifest',
             headers: { 'Authorization': 'Bearer ' + access_token },
-            success: function(res) {
+            success: function (res) {
                 if (res.status === 'success') launchViewer(urn4);
                 else $("#forgeViewer").html('Преобразование всё ещё выполняется').css('color', 'lightblue');
             }
@@ -26,7 +26,7 @@ function launchViewer(urn) {
     Autodesk.Viewing.Initializer(options, () => {
         viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), {
             extensions: ['HandleSelectionExtension', 'Markup3dExtension', 'Autodesk.Fusion360.Animation', 'Autodesk.ViewCubeUi']
-                //disabledExtensions: { explode: true, bimwalk: true, settings: true, propertiesmanager: true, modelstructure: true }
+            //disabledExtensions: { explode: true, bimwalk: true, settings: true, propertiesmanager: true, modelstructure: true }
         });
         viewer.start();
         viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
@@ -79,15 +79,11 @@ function onDocumentLoadSuccess(doc) {
 
 }
 
-
-
-var default_cam = {};
-
 function treeEvents() {
     let isolated;
     let lastNode;
 
-    $("#compTree").on("open_node.jstree", function(e, data) {
+    $("#compTree").on("open_node.jstree", function (e, data) {
         if (data.node.id === 'components' || data.node.id === 'service') {
             var row = $(".row").children();
             $(row[0]).removeClass('col-sm-2 col-md-2').addClass('col-sm-3 col-md-3');
@@ -95,7 +91,7 @@ function treeEvents() {
         }
     });
 
-    $("#compTree").on("close_node.jstree", function(e, data) {
+    $("#compTree").on("close_node.jstree", function (e, data) {
         if (data.node.id === 'components' || data.node.id === 'service') {
             var row = $(".row").children();
             $(row[0]).removeClass('col-sm-3 col-md-3').addClass('col-sm-2 col-md-2');
@@ -103,28 +99,18 @@ function treeEvents() {
         }
     });
 
-    $('#compTree').on("activate_node.jstree", function(evt, data) {
+    $('#compTree').on("activate_node.jstree", function (evt, data) {
         if (data != null && data.node != null) {
             $.ajax({
                 url: '/model_id',
                 type: 'GET',
                 data: { 'type': data.node.type },
-                success: function(res) {
+                success: function (res) {
                     getModel(Number(res));
                     $('#toolbar-animation-Close').click();
-                    // console.log(viewer.getCamera());
-                    // var front = new THREE.Vector3(-1, 1, 1);
-                    // viewer.navigation.setPosition(front);
-                    // console.log(default_cam.targ);
-                    // viewer.navigation.setView(new THREE.Vector3(398.8670230375671, 411.69096333159155, 552.282290329431), new THREE.Vector3(66.29308499955056, 6.522734619080261, -41.57116821646642));
                     $('.homeViewWrapper').click();
-                    // cube = viewer.getExtension('Autodesk.ViewCubeUi');
-                    // cube.setViewCube('front top right');
-                    // $("#forgeViewer").on("click", (e) => {
-                    //     console.log(viewer.navigation.getPosition(), viewer.navigation.getTarget());
-                    // })
                 },
-                error: function(err) {
+                error: function (err) {
                     console.log(err);
                 }
             }).then(() => {
@@ -136,12 +122,12 @@ function treeEvents() {
                         $.ajax({
                             url: '/texts/' + dbid,
                             type: 'GET',
-                            success: function(res) {
+                            success: function (res) {
                                 let name = res.name;
                                 let text = res.text;
                                 adjustLayout(name, text);
                             },
-                            error: function(err) {
+                            error: function (err) {
                                 console.log(err);
                             }
                         })
@@ -158,12 +144,12 @@ function treeEvents() {
                         url: '/tree/texts',
                         type: 'GET',
                         data: { 'id': data.node.id },
-                        success: function(res) {
+                        success: function (res) {
                             let name = res.name;
                             let text = res.text;
                             adjustLayout(name, text);
                         },
-                        error: function(err) {
+                        error: function (err) {
                             console.log(err);
                         }
                     });
@@ -175,20 +161,10 @@ function treeEvents() {
     });
 };
 
-
-function animClick(id) {
-    $(`#${id}`).on('click', () => {
-        aExt = viewer.getExtension('Autodesk.Fusion360.Animation');
-        console.log("Петр");
-        aExt.load();
-        aExt.play();
-    });
-};
 var loadedId;
 
 function getModel(id) {
     let modelArray = [1, 2, 3, 4, 5];
-    let aExt;
     for (item of modelArray) {
         var scene = viewer.impl.modelQueue();
         var model = scene.findModel(item);
@@ -218,7 +194,7 @@ function getAlldbIds(rootId) {
     while (queue.length > 0) {
         var node = queue.shift();
         alldbId.push(node);
-        instanceTree.enumNodeChildren(node, function(childrenIds) {
+        instanceTree.enumNodeChildren(node, function (childrenIds) {
             queue.push(childrenIds);
         });
     }
--- public/js/compTree.js
+++ public/js/compTree.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     prepareTree();
     $.ajax({
         url: '/tree',
         type: 'GET',
-        success: function(res) {
+        success: function (res) {
             for (item in res) {
                 $('#compTree').jstree(true).settings.core.data[item] = res[item];
             }
             $('#compTree').jstree(true).refresh();
 
         },
-        error: function(err) {
+        error: function (err) {
             console.log(err);
         }
     });
@@ -57,7 +57,7 @@ function buildModelTree(model, createNodeFunc = null) {
     //builds model tree recursively
     function _buildModelTreeRec(node) {
         instanceTree.enumNodeChildren(node.dbId,
-            function(childId) {
+            function (childId) {
                 var childNode = null;
                 if (createNodeFunc) {
                     childNode = createNodeFunc(childId);
@@ -132,24 +132,70 @@ function adjustLayout(name, text) {
         setTimeout(() => {
             $('#textboard').addClass('slide-pos');
             $('#textInfo').html(`<p class="headtext">${name}</p><p class="maintext">${text}</p>`);
-            let btn = $('#textboard').find("button")[0];
-            console.log("Илья");
-            if (btn) {
-                console.log(btn);
-                animClick(btn.id);
+            let btnStart = $('#textboard').find("button")[0];
+            let btnResume = $('#textboard').find("button")[1];
+            let btnStop = $('#textboard').find("button")[2];
+            if (btnStart) {
+                animStart(btnStart.id);
             }
+            if (btnResume)
+                animResume(btnResume.id);
+            if (btnStop)
+                animStop(btnStop.id);
         }, 400);
     } else {
         $('#textboard').html('<div class="panel panel-default"><div class="textInfo" id="textInfo"></div></div>');
         $('#textboard').addClass('slide-pos');
         $('#textInfo').html(`<p class="headtext">${name}</p>${text}`);
-        let btn = $('#textboard').find("button")[0];
-        console.log("Илья");
-        if (btn) {
-            console.log(btn);
-            animClick(btn.id);
+        let btnStart = $('#textboard').find("button")[0];
+        if (btnStart) {
+            animStart(btnStart.id);
         }
     }
 
     // $('#textboard').html('<div class="panel panel-default"><div class="textInfo"><p class="headtext">Оппозитный двигатель</p><p class="maintext">&nbsp;&nbsp;&nbsp;&nbsp;Устройство и работа механизма: Оппозитный двигатель построен на принципе двойного сжатия и включает в себя основные элементы: <span class="object" id="korpus">Корпус (М1.01.00.001)</span>, <span class="object" id="poddon">Поддон (М1.01.00.002)</span>,<span class="object" id="cilinder"> Цилиндр (М1.01.00.003)</span>, <span class="object" id="perehodnik">Переходник (М1.01.00.004)</span>, <span class="object" id="pgr">Поршневая группа (М1.01.00.200)</span>. В свою очередь в Поршневую группу входят 2 сборочные единицы: Шатун в сборе (М1.01.00.220) и Поршень малый в сборе (М1.01.00.210). Вращение <span class="object" id="porschen">Поршня (М1.01.00.211)</span> вокруг оси <span class="object" id="prou">проушины Шатуна (М1.01.00.202)</span> в сборочной единице М1.01.00.210 осуществляется за счет свободной посадки на <span class="object" id="palec">Палец (М1.01.00.213)</span>, для исключения износа поверхности Шатуна и Поршня предусмотрена установка между ними <span class="object" id="kolco212">Кольца (М1.01.00.212)</span>. Ограничение осевых перемещений Пальца осуществляется за счет <span class="object" id="kolcoA17">Кольца А17.60С2А ГОСТ 13942-86</span>. Сборочная единица М1.01.00.220 состоит из <span class="object" id="shatun">Шатуна (М1.01.00.202-01)</span> и запрессованного в него <span class="object" id="sharik">шарикоподшипника 60103 ГОСТ 7242-81</span> для обеспечения вращения поршня. Запуск двигателя осуществляется при помощи стартер-генератора, соединенного с двигателем через шлицевую муфту со шлицем d-10х21Н7х26х3 ГОСТ1139-80. Подача топлива в Цилиндры осуществляется совместно с воздухом в виде топливовоздушной смеси через <span class="object" id="patrubokvp">Патрубок впрыска (М1.01.00.110)</span>. Выход продуктов сгорания осуществляется через <span class="object" id="patrubokv">Патрубок выхлопной (М1.01.00.100)</span>. Работа цилиндров осуществляется поочередно в противоходе, открытие впускных и выпускных каналов происходит за счет поступательного движения поршней. Работа внутренних механизмов осуществляется в масленом тумане, создаваемом форсунками, установленными в бобышки верхней части двигателя. Для слива масла вовремя ТО осуществляется с помощью пробки, которая расположена в нижней части Поддона.</p></div></div>');
+}
+
+var aExt;
+var checkCurTime;
+
+function animStart(id) {
+    $(`#${id}`).on('click', () => {
+        aExt = viewer.getExtension('Autodesk.Fusion360.Animation');
+        if (!aExt.isPlaying()) {
+            aExt.load();
+            aExt.play();
+            checkCurTime = setInterval(() => {
+                changePlugInstruction();
+            }, 250);
+        } else {
+            if (checkCurTime != 'undefined') {
+                clearInterval(checkCurTime);
+                aExt.pause();
+            }
+        }
+    });
+}
+
+function changePlugInstruction() {
+
+    let span = $('.maintext').children();
+    if (aExt.getCurrentTime() > 0 && aExt.getCurrentTime() < 5) {
+        $(span[0]).addClass('activeText');
+        $(span[1]).removeClass('activeText');
+        $(span[2]).removeClass('activeText');
+    }
+    if (aExt.getCurrentTime() >= 5 && aExt.getCurrentTime() < 10) {
+        $(span[1]).addClass('activeText');
+        $(span[0]).removeClass('activeText');
+        $(span[2]).removeClass('activeText');
+    }
+    if (aExt.getCurrentTime() >= 10 && aExt.getCurrentTime() < aExt.getDuration()) {
+        $(span[2]).addClass('activeText');
+        $(span[0]).removeClass('activeText');
+        $(span[1]).removeClass('activeText');
+    }
+    if (aExt.isPaused()) {
+        return;
+    }
 }
