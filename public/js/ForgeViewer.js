244d1e9a49ad2d776b94b86d57a4558d1e040f28
Marcusbille <60420179+Marcusbille@users.noreply.github.com>
2020-05-16 15:28:13 +0300

    changesModels_fixes_V2

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -1,5 +1,5 @@
 var viewer;
-$(document).ready(function () {
+$(document).ready(function() {
     $("#forgeViewer").empty();
     var urn = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZS5zdHA=';
     var urn2 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL1Rlc3QuZjNk';
@@ -8,11 +8,11 @@ $(document).ready(function () {
     var urn5 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZTIxMjMxLmYzZA==';
     var urn6 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZVJseUxhc3QuZjNk';
     var urn7 = 'dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6dDhkN3h2anZkY2VjdWx3eXN6ZmVpaWg1ZXZ0Z3RqYm8tZW5naW5lL0VuZ2luZVYyLmYzZA==';
-    getForgeToken(function (access_token) {
+    getForgeToken(function(access_token) {
         jQuery.ajax({
             url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/' + urn7 + '/manifest',
             headers: { 'Authorization': 'Bearer ' + access_token },
-            success: function (res) {
+            success: function(res) {
                 if (res.status === 'success') launchViewer(urn7);
                 else $("#forgeViewer").html('Преобразование всё ещё выполняется').css('color', 'lightblue');
             }
@@ -29,7 +29,7 @@ function launchViewer(urn) {
     Autodesk.Viewing.Initializer(options, () => {
         viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), {
             extensions: ['HandleSelectionExtension', 'Markup3dExtension', 'Autodesk.Fusion360.Animation', 'Autodesk.ViewCubeUi']
-            //disabledExtensions: { explode: true, bimwalk: true, settings: true, propertiesmanager: true, modelstructure: true }
+                //disabledExtensions: { explode: true, bimwalk: true, settings: true, propertiesmanager: true, modelstructure: true }
         });
         viewer.start();
         viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
@@ -89,7 +89,7 @@ function treeEvents() {
     let isolated;
     let lastNode;
 
-    $("#compTree").on("open_node.jstree", function (e, data) {
+    $("#compTree").on("open_node.jstree", function(e, data) {
         if (data.node.id === 'components' || data.node.id === 'service') {
             var row = $(".row").children();
             $(row[0]).removeClass('col-sm-2 col-md-2').addClass('col-sm-3 col-md-3');
@@ -97,7 +97,7 @@ function treeEvents() {
         }
     });
 
-    $("#compTree").on("close_node.jstree", function (e, data) {
+    $("#compTree").on("close_node.jstree", function(e, data) {
         if ((data.node.id === 'components' || data.node.id === 'service') && !$("#compTree").jstree(true).get_node("components").state.opened && !$("#compTree").jstree(true).get_node("service").state.opened) {
             var row = $(".row").children();
             $(row[0]).removeClass('col-sm-3 col-md-3').addClass('col-sm-2 col-md-2');
@@ -105,19 +105,19 @@ function treeEvents() {
         }
     });
 
-    $('#compTree').on("activate_node.jstree", function (evt, data) {
+    $('#compTree').on("activate_node.jstree", function(evt, data) {
         if (data != null && data.node != null) {
             $.ajax({
                 url: '/model_id',
                 type: 'GET',
                 data: { 'type': data.node.type },
-                success: function (res) {
+                success: function(res) {
                     console.log(res);
                     getModel(Number(res));
                     $('#toolbar-animation-Close').click();
                     $('.homeViewWrapper').click();
                 },
-                error: function (err) {
+                error: function(err) {
                     console.log(err);
                 }
             }).then(() => {
@@ -129,12 +129,12 @@ function treeEvents() {
                         $.ajax({
                             url: '/texts/' + dbid,
                             type: 'GET',
-                            success: function (res) {
+                            success: function(res) {
                                 let name = res.name;
                                 let text = res.text;
                                 adjustLayout(name, text);
                             },
-                            error: function (err) {
+                            error: function(err) {
                                 console.log(err);
                             }
                         })
@@ -151,12 +151,12 @@ function treeEvents() {
                         url: '/tree/texts',
                         type: 'GET',
                         data: { 'id': data.node.id },
-                        success: function (res) {
+                        success: function(res) {
                             let name = res.name;
                             let text = res.text;
                             adjustLayout(name, text);
                         },
-                        error: function (err) {
+                        error: function(err) {
                             console.log(err);
                         }
                     });
@@ -171,41 +171,60 @@ function treeEvents() {
 let curModel;
 
 function getModel(time) {
-    let modelArray = [1, 2, 3, 4, 5];
-    let scene = viewer.impl.modelQueue();
-    console.log(scene.findModel(1));
-    console.log(scene.findModel(2));
-    console.log(scene.findModel(3));
-    console.log(scene.findModel(4));
-    for (item of modelArray) {
-        let model = scene.findModel(item);
-        if (model != null) {
-            if (model.myData.animations) {
-                if (model.myData.animations.duration !== time) {
-                    // viewer.impl.removeModel(model);
-                    // scene.addHiddenModel(model);
-                    viewer.hideModel(model.id);
-                } else {
-                    if (curModel !== time) {
-                        curModel = time;
-                        console.log(model.id);
-                        viewer.showModel(model.id);
-                        viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
-                    }
-                }
-            } else {
-                if (time !== 1) {
-                    // viewer.impl.removeModel(model);
-                    // scene.addHiddenModel(model);
-                    viewer.hideModel(model.id);
-                } else {
-                    curModel = time;
-                    viewer.showModel(model.id);
-                    viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
-                }
+    let modelArray = [1, 2, 3, 4];
+    let visible = viewer.getVisibleModels();
+    let models = visible.concat(viewer.getHiddenModels());
+    console.log(models);
+    for (let item of models) {
+        if (item.myData.animations) {
+            if (item.myData.animations.duration !== time) {
+                viewer.hideModel(item.id);
+            } else if (curModel !== item) {
+                console.log(item.myData.animations.duration);
+                curModel = item;
+                viewer.showModel(item.id);
+                viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
             }
+        } else if (time == 1 && curModel !== item) {
+            curModel = item;
+            viewer.showModel(item.id);
+            viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
+        } else {
+            viewer.hideModel(item.id);
         }
+
     }
+
+
+    // for (item of modelArray) {
+    //     let scene = viewer.impl.modelQueue();
+    //     let model = scene.findModel(item);
+
+    //     if (model.myData.animations) {
+    //         if (model.myData.animations.duration !== time) {
+    //             viewer.impl.removeModel(model);
+    //             scene.addHiddenModel(model);
+    //             // viewer.hideModel(model.id);
+    //         } else {
+    //             if (curModel !== time) {
+    //                 curModel = time;
+    //                 console.log(model.id);
+    //                 viewer.showModel(model.id);
+    //                 viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
+    //             }
+    //         }
+    //     } else {
+    //         if (time == 1 && curModel !== time) {
+    //             curModel = time;
+    //             viewer.showModel(model.id);
+    //             viewer.setBackgroundColor(242, 242, 242, 242, 242, 242);
+    //         } else {
+    //             viewer.impl.removeModel(model);
+    //             scene.addHiddenModel(model);
+    //         }
+    //     }
+
+    // }
 }
 
 
@@ -219,7 +238,7 @@ function getAlldbIds(rootId) {
     while (queue.length > 0) {
         var node = queue.shift();
         alldbId.push(node);
-        instanceTree.enumNodeChildren(node, function (childrenIds) {
+        instanceTree.enumNodeChildren(node, function(childrenIds) {
             queue.push(childrenIds);
         });
     }
