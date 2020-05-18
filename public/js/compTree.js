871f0cd1832c74fbc51f33c04cf5373fd9e11a60
Nezar <hamideh@yandex.ru>
2020-05-18 23:30:27 +0300

    buttonsAnimationsV1

--- public/js/compTree.js
+++ public/js/compTree.js
@@ -1,16 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     prepareTree();
     $.ajax({
         url: '/tree',
         type: 'GET',
-        success: function(res) {
+        success: function (res) {
             for (item in res) {
                 $('#compTree').jstree(true).settings.core.data[item] = res[item];
             }
             $('#compTree').jstree(true).refresh();
 
         },
-        error: function(err) {
+        error: function (err) {
             console.log(err);
         }
     });
@@ -57,7 +57,7 @@ function buildModelTree(model, createNodeFunc = null) {
     //builds model tree recursively
     function _buildModelTreeRec(node) {
         instanceTree.enumNodeChildren(node.dbId,
-            function(childId) {
+            function (childId) {
                 var childNode = null;
                 if (createNodeFunc) {
                     childNode = createNodeFunc(childId);
@@ -156,22 +156,22 @@ let timePoints;
 let steps;
 let curTimePoint = 0;
 let instructions = [{
-        'id': 'anim_changePlug',
-        'time': [
-            [2, 5],
-            [5, 10],
-            [10, 12.63]
-        ]
-    },
-    {
-        'id': 'anim_changeGasket',
-        'time': [
-            [2, 5],
-            [5, 10],
-            [10, 16],
-            [16, 24],
-        ]
-    }
+    'id': 'anim_changePlug',
+    'time': [
+        [2, 5],
+        [5, 10],
+        [10, 12.63]
+    ]
+},
+{
+    'id': 'anim_changeGasket',
+    'time': [
+        [2, 5],
+        [5, 10],
+        [10, 16],
+        [16, 24],
+    ]
+}
 ];
 
 function animStart(id) {
@@ -193,6 +193,22 @@ function animStart(id) {
             }
             aExt.load();
             aExt.play();
+            if (id === 'anim_info') {
+                $('#animQuit').css('display', 'inline-block');
+                $('#animQuit').on('click', () => {
+                    //////////
+                });
+            }
+            $('#animToggle').css('display', 'inline-block');
+            $('#animToggle').on('click', () => {
+                if (aExt.isPlaying()) {
+                    $('#animToggle').html('Продолжить анимацию');
+                    aExt.pause();
+                } else {
+                    $('#animToggle').html('Остановить анимацию');
+                    aExt.play();
+                }
+            });
             if (timePoints && steps.length > 0) {
                 checkCurTime = setInterval(() => {
                     changeInstruction();
